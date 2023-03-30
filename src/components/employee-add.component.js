15eeeeb40edae8a315962a344a77e7229b8b80ef
kalanarathnayake <kalanagayanga8@gmail.com>
2023-03-31 02:34:02 +0530

    Feat: Add employee page ui completed

--- src/components/employee-add.component.js
+++ src/components/employee-add.component.js
@@ -1,77 +1,65 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import * as Swal from "sweetalert2";
 
-
-
 export class CreateEmployee extends Component {
-
-    
-    constructor(props){
+    constructor(props) {
         super(props);
-
-        
-       
         this.onChangeempID = this.onChangeempID.bind(this);
         this.onChangefullName = this.onChangefullName.bind(this);
         this.onChangecontactNo = this.onChangecontactNo.bind(this);
         this.onChangeemail = this.onChangeemail.bind(this);
         this.onChangeaddress = this.onChangeaddress.bind(this);
         this.onChangeposition = this.onChangeposition.bind(this);
-        
-
         this.onSubmit = this.onSubmit.bind(this);
-        
-       
 
         this.state = {
-            empID : '',
-            fullName : '',
-            contactNo : '',
-            email : '',
-            address : '',
-            position : ''
-            
+            empID: '',
+            fullName: '',
+            contactNo: '',
+            email: '',
+            address: '',
+            position: ''
         }
     }
 
-    onChangeempID(e){
+    onChangeempID(e) {
         this.setState({
-            empID : e.target.value
+            empID: e.target.value
         });
     }
 
-    onChangefullName(e){
+    onChangefullName(e) {
         this.setState({
-            fullName : e.target.value
+            fullName: e.target.value
         });
     }
 
-    onChangecontactNo(e){
+    onChangecontactNo(e) {
         this.setState({
-            contactNo : e.target.value
+            contactNo: e.target.value
         });
     }
 
-    onChangeemail(e){
+    onChangeemail(e) {
         this.setState({
-            email : e.target.value
+            email: e.target.value
         });
     }
 
-    onChangeaddress(e){
+    onChangeaddress(e) {
         this.setState({
-            address : e.target.value
+            address: e.target.value
         });
     }
 
-    onChangeposition(e){
+    onChangeposition(e) {
         this.setState({
-        position : e.target.value
+            position: e.target.value
         });
     }
 
-   
+
 
     // demoClicked(){
     //     this.setState({
@@ -84,21 +72,21 @@ export class CreateEmployee extends Component {
     //         address : "Gampaha",
     //         contactNo : 77564213,
     //         emergency : 76124321,
-    
-            
+
+
     //     })
     // }
 
-    onSubmit(e){
+    onSubmit(e) {
         e.preventDefault();
 
         const employee = {
-            empID : this.state.empID,
-            fullName : this.state.fullName,
-            contactNo : this.state.contactNo,
-            email : this.state.email,
-            address : this.state.address,
-            position : this.state.position
+            empID: this.state.empID,
+            fullName: this.state.fullName,
+            contactNo: this.state.contactNo,
+            email: this.state.email,
+            address: this.state.address,
+            position: this.state.position
         }
 
         console.log(employee);
@@ -112,42 +100,40 @@ export class CreateEmployee extends Component {
         // if(this.state.contactNo.length != 10){
         //     this.setState({contactNoError : "Contact Number is invalid."})
         // }
-       
+
         // else if(this.state.fullName.length >= 10  && this.state.empID.length == 4)
         // {
-            axios.post('http://localhost:5000/employee/', employee)
-        // .then(res => console.log("success")).catch(err=>console.log(err));
+        axios.post('http://localhost:5000/employee/', employee)
+            // .then(res => console.log("success")).catch(err=>console.log(err));
 
-        .then(res => {
-            
-            console.log(res);
+            .then(res => {
 
-            if (res.status === 200) {
-                this.clearData();
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Successful',
-                    text: 'Classroom has been added!!',
-                    background: '#fff',
-                    confirmButtonColor: '#333533',
-                    iconColor: '#60e004'
-                })
+                console.log(res);
 
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Error in adding!',
-                    background: '#fff',
-                    confirmButtonColor: '#333533',
-                    iconColor: '#e00404'
-                })
-            }
-        })
+                if (res.status === 200) {
+                    this.clearData();
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Successful',
+                        text: 'Classroom has been added!!',
+                        background: '#fff',
+                        confirmButtonColor: '#333533',
+                        iconColor: '#60e004'
+                    })
 
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: 'Error in adding!',
+                        background: '#fff',
+                        confirmButtonColor: '#333533',
+                        iconColor: '#e00404'
+                    })
+                }
+            })
         // window.location = '/';
         // }
-        
     }
 
     clearData = () => {
@@ -157,113 +143,113 @@ export class CreateEmployee extends Component {
             contactNo: '',
             email: '',
             address: '',
-            position : ''
+            position: ''
         })
     }
 
-    
-
-    
-
-    
-
     render() {
         return (
-        <div >
-            
-                <div>
-                
-            <h3 >Add Employee</h3>
-        
-                <form onSubmit = {this.onSubmit}>
-                    <div className = "form-group">
-                        <label>Employee ID : </label>
-                        <input type = "text"
-                        required
-                        className = "form-control"
-                        value = {this.state.empID}
-                        onChange = {this.onChangeempID}
-                        />
-                        <p className = "validateMsg">{this.state.empIDError}</p>
-                    </div>
+            <div className="flex flex-col px-5 pt-2 ">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                        <div className='items-center overflow-hidden'>
+                            <div className=''>
+                                <div class="grid grid-cols-1 gap-4 content-start pt-5 px-20">
+                                    <form className='px-12 py-12 border-2 rounded-lg shadow-md bg-gray-50' onSubmit={this.onSubmit}>
+                                        <div class="">
+                                            <p className='text-4xl font-semibold text-black uppercase'>Add Employee</p>
+                                            <div className="grid grid-cols-2 gap-4 form-group">
+                                                <div class="">
+                                                    <label className='block mb-2 text-lg font-medium text-gray-900 dark:text-white'>Employee ID : </label>
+                                                    <input type="text"
+                                                        required
+                                                        className="form-control "
+                                                        value={this.state.empID}
+                                                        onChange={this.onChangeempID}
+                                                    />
+                                                    <p className="validateMsg">{this.state.empIDError}</p>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label className='block mb-2 text-lg font-medium text-gray-900 dark:text-white'>Full Name : </label>
+                                                    <input type="text"
+                                                        required
+                                                        className="form-control"
+                                                        value={this.state.fullName}
+                                                        onChange={this.onChangefullName}
+                                                    />
+                                                    <p className="validateMsg">{this.state.nameError}</p>
+                                                </div>
+                                            </div>
 
-                    <div className = "form-group">
-                        <label>Full Name : </label>
-                        <input type = "text"
-                        required
-                        className = "form-control"
-                        value = {this.state.fullName}
-                        onChange = {this.onChangefullName}
-                        />
-                        <p className = "validateMsg">{this.state.nameError}</p>
-                    </div>
+                                            <div className="grid grid-cols-2 gap-4 form-group">
+                                                <div class="">
+                                                    <label className='block mb-2 text-lg font-medium text-gray-900 dark:text-white' >Contact Number : </label>
+                                                    <input type="text"
+                                                        required
+                                                        className="form-control"
+                                                        value={this.state.contactNo}
+                                                        onChange={this.onChangecontactNo}
+                                                    />
+                                                    <p className="validateMsg">{this.state.contactNoError}</p>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label className='block mb-2 text-lg font-medium text-gray-900 dark:text-white'>Eamil : </label>
+                                                    <input type="text"
+                                                        required
+                                                        className="form-control"
+                                                        value={this.state.email}
+                                                        onChange={this.onChangeemail}
+                                                    />
+                                                </div>
+                                            </div>
+                                            {/* <div className = "form-group">
+                                            <label>DOB : </label>
+                                            <div>
+                                                <DatePicker
+                                                selected = {this.state.dob}
+                                                onChange = {this.onChangedob}
+                                                />
+                                            </div>
+                                        </div> */}
 
-                    <div className = "form-group">
-                        <label>Contact Number : </label>
-                        <input type = "text"
-                        required
-                        className = "form-control"
-                        value = {this.state.contactNo}
-                        onChange = {this.onChangecontactNo}
-                        />
-                        <p className = "validateMsg">{this.state.contactNoError}</p>
-                    </div>
-                    {/* <div className = "form-group">
-                        <label>DOB : </label>
-                        <div>
-                            <DatePicker
-                            selected = {this.state.dob}
-                            onChange = {this.onChangedob}
-                            />
+                                            <div className="form-group">
+                                                <label for="large-input" className='block mb-2 text-lg font-medium text-gray-900 dark:text-white'>Address : </label>
+                                                <input type="text"
+                                                    required
+                                                    className="form-control"
+                                                    value={this.state.address}
+                                                    onChange={this.onChangeaddress}
+                                                /><p> </p>
+                                            </div>
+                                            <div className="form-group ">
+                                                <label className='block mb-2 text-lg font-medium text-gray-900 dark:text-white' for="grid-state">Position : </label>
+                                                <select type="text"
+                                                    required
+                                                    className="form-control"
+                                                    value={this.state.position}
+                                                    onChange={this.onChangeposition}
+                                                >
+                                                    <option>Waiter Staff</option>
+                                                    <option>Kitchen Head Chef</option>
+                                                    <option>Inventory Manager</option>
+                                                    <option>Driver</option>
+                                                    <option>Delivery Manager</option>
+                                                    <option>Employee Manager</option>
+                                                    <option>Finantial Manager</option>
+                                                    <option>Product Manager</option>
+                                                </select><p></p>
+                                            </div>
+                                            <div className="text-center align-middle form-group">
+                                                <input className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' type="submit" value="Add Employee" />
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
-                    </div> */}
-                    <div className = "form-group">
-                        <label>Eamil : </label>
-                        <input type = "text"
-                        required
-                        className = "form-control"
-                        value = {this.state.email}
-                        onChange = {this.onChangeemail}
-                        />
                     </div>
-                    <div className = "form-group">
-                        <label>Address : </label>
-                        <input type = "text"
-                        required
-                        className = "form-control"
-                        value = {this.state.address}
-                        onChange = {this.onChangeaddress}
-                        />
-                    </div>
-                    <div className = "form-group">
-                        <label>Position : </label>
-                        <input type = "text"
-                        required
-                        className = "form-control"
-                        value = {this.state.position}
-                        onChange = {this.onChangeposition}
-                        />
-                    </div>
-
-
-
-                    <div className = "form-group">
-                        <input type = "submit" value = "Add Employee" />
-                    </div>
-                </form>
-
-               
-
-
-</div>
-
-                
-
-    </div>
-
-                
-
-                
+                </div>
+            </div>
         )
     }
 }
